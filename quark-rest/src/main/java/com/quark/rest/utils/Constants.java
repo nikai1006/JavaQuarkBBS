0100be35a12ad449b1cd2285a7cf48ea3572c92f
nikai <nikai1006@126.com>
2020-09-16 18:34:55 +0800

    update

--- quark-rest/src/main/java/com/quark/rest/utils/Constants.java
+++ quark-rest/src/main/java/com/quark/rest/utils/Constants.java
@@ -6,7 +6,7 @@ package com.quark.rest.utils;
  */
 public class Constants {
     //Upload常量
-    public static final String UPLOAD_PATH = "D:\\home\\images\\upload\\";
-    public static final String STATIC_URL ="http://127.0.0.1/images/upload/";
+    public static final String UPLOAD_PATH = "D:\\server\\nginx\\web-workspace\\bbs-front\\images\\upload\\";
+    public static final String STATIC_URL ="http://192.168.14.9/images/upload/";
 
 }
--- quark-rest/src/main/java/com/quark/rest/utils/FileUtils.java
+++ quark-rest/src/main/java/com/quark/rest/utils/FileUtils.java
@@ -1,14 +1,15 @@
 package com.quark.rest.utils;
 
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.UUID;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
- * @Author LHR
- * Create By 2017/8/26
+ * @Author LHR Create By 2017/8/26
  */
 public class FileUtils {
 
@@ -19,15 +20,18 @@ public class FileUtils {
             String type = file.getContentType();
             String suffix = "." + type.split("/")[1];
             String userUploadPath = today + "/";
-            String fileName = UUID.randomUUID().toString()+suffix;
+            String fileName = UUID.randomUUID().toString() + suffix;
             File file_dir = new File(Constants.UPLOAD_PATH + userUploadPath);
-            if (!file_dir.exists()) file_dir.mkdirs();
+            if (!file_dir.exists()) {
+                file_dir.mkdirs();
+            }
 
-            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(Constants.UPLOAD_PATH + userUploadPath + fileName)));
+            BufferedOutputStream stream = new BufferedOutputStream(
+                new FileOutputStream(new File(Constants.UPLOAD_PATH + userUploadPath + fileName)));
             stream.write(file.getBytes());
             stream.close();
 
-            return Constants.STATIC_URL+userUploadPath+fileName;
+            return Constants.STATIC_URL + userUploadPath + fileName;
         }
         return null;
     }
